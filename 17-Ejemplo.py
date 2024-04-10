9c852d92605a338bac7578965a81ca272b8b0d5f
Angel591Diaz <angelgabrield365@gmail.com>
2024-04-10 10:18:16 -0600

    Usando dot en el ejemplo17 y agregando nuevo ejemplo

--- 17-Ejemplo.py
+++ 17-Ejemplo.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 inputs = [ 1, 2, 3, 2.5]
 weights1 = [0.2, 0.8, -0.5, 1]
 weights2 = [0.5, -0.91, 0.26, -0.5]
@@ -5,6 +7,13 @@ weights3 = [-0.26, -0.27, 0.17, 0.87]
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
+
+a = np.dot(inputs, weights1) + bias1
+b = np.dot(inputs, weights2) + bias2
+c = np.dot(inputs, weights3) + bias3
+
+outputs2 = [a,b,c]
+
 outputs = [
 # Neuron 1:
 inputs[ 0 ] * weights1[ 0 ] +
@@ -25,4 +34,6 @@ inputs[ 3 ] * weights3[ 3 ] + bias3,
 ]
 #sumatoria
 #output = (inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias)
+
 print (outputs)
+print(outputs2)
