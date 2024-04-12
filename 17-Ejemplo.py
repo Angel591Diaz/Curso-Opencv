52be9a1013a2d048b660b1354654bedeb3cd5da8
Angel591Diaz <angelgabrield365@gmail.com>
2024-04-12 17:32:56 -0600

    Creacion de ejemplo 18 y 19 y actualización del ejemplo 17

--- 17-Ejemplo.py
+++ 17-Ejemplo.py
@@ -1,20 +1,15 @@
 import numpy as np
 
-inputs = [ 1, 2, 3, 2.5]
-weights1 = [0.2, 0.8, -0.5, 1]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+inputs = [ 1, 1, 2, 3, 2.5]
 bias1 = 2
 bias2 = 3
 bias3 = 0.5
+weights1 = [bias1, 0.2, 0.8, -0.5, 1]
+weights2 = [bias2, 0.5, -0.91, 0.26, -0.5]
+weights3 = [bias3, -0.26, -0.27, 0.17, 0.87]
 
-a = np.dot(inputs, weights1) + bias1
-b = np.dot(inputs, weights2) + bias2
-c = np.dot(inputs, weights3) + bias3
 
-outputs2 = [a,b,c]
-
-outputs = [
+""" outputs1 = [
 # Neuron 1:
 inputs[ 0 ] * weights1[ 0 ] +
 inputs[ 1 ] * weights1[ 1 ] +
@@ -31,9 +26,13 @@ inputs[ 1 ] * weights3[ 1 ] +
 inputs[ 2 ] * weights3[ 2 ] +
 inputs[ 3 ] * weights3[ 3 ] + bias3,
 
-]
-#sumatoria
-#output = (inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias)
+] """
 
-print (outputs)
-print(outputs2)
+outputs2 = [
+    np.dot(inputs, weights1),
+    np.dot(inputs, weights2),
+    np.dot(inputs, weights3)
+]
+
+#print (outputs1)
+print (outputs2)
